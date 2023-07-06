9040576e014e1dbef90d965dc0926f05fc23749e
helkarakse <zheyanglee@gmail.com>
2023-07-07 00:01:20 +0800

    Moved instance configuration into separate property

--- src/Classes/ValueObjects/Configuration.ts
+++ src/Classes/ValueObjects/Configuration.ts
@@ -5,20 +5,28 @@ export type BotConfiguration = {
   isLocal: boolean;
 };
 
+export type InstanceConfiguration = {
+  id: number;
+  url: string;
+};
+
 export class Configuration {
   constructor(
-    public readonly url: string,
+    public readonly instance: InstanceConfiguration,
     public readonly bot: BotConfiguration
   ) {}
 
   static createFromEnv(): Configuration {
     const instanceUrl = process.env.INSTANCE_URL || "https://my.lemmy.local";
 
-    return new Configuration(instanceUrl, {
-      instance: new URL(instanceUrl).hostname,
-      username: process.env.BOT_USERNAME || "username",
-      password: process.env.BOT_PASSWORD || "password",
-      isLocal: true,
-    });
+    return new Configuration(
+      { id: 1, url: instanceUrl },
+      {
+        instance: new URL(instanceUrl).hostname,
+        username: process.env.BOT_USERNAME || "username",
+        password: process.env.BOT_PASSWORD || "password",
+        isLocal: true,
+      }
+    );
   }
 }
