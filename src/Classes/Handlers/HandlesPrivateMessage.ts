1fddcb67ed38411770420257aef0e0b8cbc690af
helkarakse <zheyanglee@gmail.com>
2023-07-09 16:25:43 +0800

    Remove the configuration property from HandlesPrivateMessage.ts

--- src/Classes/Handlers/HandlesPrivateMessage.ts
+++ src/Classes/Handlers/HandlesPrivateMessage.ts
@@ -1,10 +1,7 @@
 import { BotActions } from "lemmy-bot";
 import { Person } from "lemmy-js-client";
-import { Configuration } from "../ValueObjects/Configuration";
 
 export interface HandlesPrivateMessage {
-  readonly configuration: Configuration;
-
   getMatchExpression(): RegExp;
 
   hasPermission(person: Person): Promise<boolean>;
