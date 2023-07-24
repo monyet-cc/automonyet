627c9a6b2954a6880e191dabb2006e0c84dbcf2f
Naomikho <khoyining9@gmail.com>
2023-07-24 15:08:43 +0800

    call db functions to perform post pin handling

--- src/Classes/Services/AutomatesFeaturedPost.ts
+++ src/Classes/Services/AutomatesFeaturedPost.ts
@@ -2,7 +2,7 @@ import { provide } from "inversify-binding-decorators";
 import moment from "moment";
 import { LemmyApi } from "../ValueObjects/LemmyApi.js";
 import { BotTask } from "lemmy-bot";
-import { PostgresClient } from "../ValueObjects/PostgresClient.js";
+import { PostgresService } from "./PostgresService.js";
 
 @provide(AutomatesFeaturedPost)
 export class AutomatesFeaturedPost {
@@ -11,15 +11,16 @@ export class AutomatesFeaturedPost {
       category: "Daily Chat Thread",
       communityName: "cafe",
       body: undefined,
-      pinLocal: true,
+      pinLocally: true,
       cronExpression: "0 0 0 * * *",
       timezone: "Asia/Kuala_Lumpur",
+      daysToPin: 1,
     },
   ];
 
   constructor(
     private readonly client: LemmyApi,
-    private readonly dbclient: PostgresClient
+    private readonly dbservice: PostgresService
   ) {}
 
   private generatePostTitle = (category: string): string => {
@@ -40,14 +41,21 @@ export class AutomatesFeaturedPost {
         cronExpression: post.cronExpression,
         timezone: post.timezone,
         doTask: async () => {
-          return this.handlePost(post.category);
+          return this.handlePostCreation(post.category);
         },
       });
     }
+    botTasks.push({
+      cronExpression: "0 0 0 * * *",
+      timezone: "Asia/Kuala_Lumpur",
+      doTask: async () => {
+        return this.removeOverduePins();
+      },
+    });
     return botTasks;
   };
 
-  public async handlePost(postCategory: string): Promise<void> {
+  public async handlePostCreation(postCategory: string): Promise<void> {
     const matchingPostType = this.postsToCreate.find((post) => {
       return post.category === postCategory;
     });
@@ -68,10 +76,28 @@ export class AutomatesFeaturedPost {
       },
       "Community"
     );
-    if (matchingPostType.pinLocal) {
+    if (matchingPostType.pinLocally) {
       await this.client.featurePost(postIdentifier, "Local", true);
     }
 
     //save postId in db
+    if (matchingPostType.daysToPin > 0) {
+      this.dbservice.setPostAutoRemoval(
+        postIdentifier,
+        matchingPostType.daysToPin,
+        matchingPostType.pinLocally
+      );
+    }
+  }
+
+  public async removeOverduePins(): Promise<void> {
+    const postsToUnpin = await this.dbservice.handleOverduePins();
+    if (postsToUnpin !== undefined) {
+      for (const post of postsToUnpin) {
+        this.client.featurePost(post.postId, "Community", false);
+        if (post.isLocallyPinned)
+          this.client.featurePost(post.postId, "Local", false);
+      }
+    }
   }
 }
