980a270833ce926fcb9f3914ea14328fde12b638
helkarakse <zheyanglee@gmail.com>
2023-08-26 14:43:20 +0800

    Added types for inversify bindings

--- src/Classes/Services/ConfiguresInversify.ts
+++ src/Classes/Services/ConfiguresInversify.ts
@@ -5,18 +5,19 @@ import { LemmyApiFactory } from "../Factories/LemmyApiFactory.js";
 import { Configuration } from "../ValueObjects/Configuration.js";
 import { LemmyApi } from "../ValueObjects/LemmyApi.js";
 import { Sequelize } from "sequelize";
-import { InversifyTypes } from "../ValueObjects/InversifyTypes.js";
 import { databaseConnection } from "../Database/ConfiguresDatabaseConnection.js";
 
 dotenv.config();
+
+const Types = {
+  DatabaseConnection: Symbol.for("DatabaseConnection"),
+};
 
 const container = new Container();
 const configuration = Configuration.createFromEnv();
 
 const dependencies = new ContainerModule((bind) => {
-  bind<Sequelize>(InversifyTypes.DatabaseConnection).toConstantValue(
-    databaseConnection
-  );
+  bind<Sequelize>(Types.DatabaseConnection).toConstantValue(databaseConnection);
 });
 
 container.bind<Configuration>(Configuration).toConstantValue(configuration);
